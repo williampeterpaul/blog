f6b86abcb773c89f15bafa158b0e09abb41d64cf
williampeterpaul <williampeterpaul@pm.me>
2020-11-26 19:24:55 +0000

    Add initial models

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from pathlib import Path
-from models import Post
-
-import argparse
-import datetime
-import os
+from models import Paragraph, List, Text, Emphasis, Bold, Header
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--upload', choices=('yes', 'no'), help='Upload')
-    args = parser.parse_args()
+    
 
-    if(args.upload == 'yes'):
-        print('Uploading')
 
-    current = Post('md5', 'date', 'title', 'body')
--- models.py
+++ models.py
@@ -1,6 +1,47 @@
-class Post:
-    def __init__(self, md5, date, title, body):
-        self.md5 = md5
-        self.date = date
-        self.title = title
-        self.body = body
+class Paragraph(object):
+    def __init__(self, items):
+        self.items = items
+
+    def __repr__(self):
+        return 'Paragraph({!r})'.format(self.items)
+
+
+class List(object):
+    def __init__(self, items):
+        self.items = items
+
+    def __repr__(self):
+        return 'List({!r})'.format(self.items)
+
+
+class Text(object):
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return 'Text({!r})'.format(self.text)
+
+
+class Emphasis(object):
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return 'Emphasis({!r})'.format(self.text)
+
+
+class Bold(object):
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return 'Bold({!r})'.format(self.text)
+
+
+class Header(object):
+    def __init__(self, level, items):
+        self.level = level
+        self.items = items
+
+    def __repr__(self):
+        return 'Header({},{!r})'.format(self.level, self.items)
